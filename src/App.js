56f91016f1c10816cb01d8dbea35f29dbba3f2d5
Mike Henry <mhenry16@u.rochester.edu>
2020-04-07 18:28:26 -0400

    content

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { SocialIcon } from 'react-social-icons';
+import me from './images/michaelhenry.jpeg';
+import resume from  './images/Resume.pdf'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="top">
+        <div className='social-icons'>
+        <h1 className="myName">Michael Henry</h1>
+
+        <SocialIcon url='mailto:mhenry16@u.rochester.edu' bgColor= "#5b0E2D" style={{ height: 25, width: 25}} />
+        <SocialIcon url='https://www.facebook.com/michael.henry.549' bgColor= "#5b0E2D" style={{ height: 25, width: 25 }} />
+        <SocialIcon url='https://www.linkedin.com/in/michael-henry-21853a121/' bgColor= "#5b0E2D" style={{ height: 25, width: 25 }} />
+        <SocialIcon url='https://github.com/mhenry16' bgColor= "#5b0E2D" style={{ height: 25, width: 25 }} />
+
+        </div>
+      </div>
+      <div>
+        <img className='picOfMe' src={me} alt='' />
+      </div>
+      <div className="bottom">
+        <div className='content'>
+        <p className='textAboutMe'>A software developer who does cool stuff sometimes, check out my resume <a href={resume} target='_blank' rel="noopener noreferrer">here</a></p>
+        </div>
+      </div>
     </div>
   );
 }
